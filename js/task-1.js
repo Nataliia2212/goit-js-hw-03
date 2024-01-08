69ce09669fd947c83ced77faea060bbaceee95c9
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-01-08 19:01:24 +0200

    Revert "ffinish"
    This reverts commit 767aa55e5d1956e3a7740327b00c039291b86684.

--- js/task-1.js
+++ js/task-1.js
@@ -1,27 +1,8 @@
-// function slugify(title) {
-// 	return title.toLowerCase().split(" ").join('-')
-// }
+function slugify(title) {
+	return title.toLowerCase().split(" ").join('-')
+}
 
-// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-// console.log(slugify("English for developer")); // "english-for-developer"
-// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
-};
-
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location.country = "Jamaica";
-apartment.location.city = "Kingston";
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
